124d509dc7c8e7c0b7d2b6bdb94ee7e4d312edbb
Bastien MUNOZ <bastien.munoz@aramisauto.com>
2025-02-26 09:57:23 +0100

    nvim: move config from init.vim to init.lua

---
+++ config/nvim/init.lua
@@ -0,0 +1,10 @@
+-- Remapping for Dvorak
+vim.keymap.set({'n', 's', 'o', 'x'}, 't', 'k')
+vim.keymap.set({'n', 's', 'o', 'x'}, 'n', 'j')
+vim.keymap.set({'n', 's', 'o', 'x'}, 's', 'l')
+vim.keymap.set({'n', 's', 'o', 'x'}, 'j', 'n')
+vim.keymap.set({'n', 's', 'o', 'x'}, 'k', 's')
+
+-- Enable relative numbers
+vim.wo.relativenumber = true
+
--- config/nvim/init.vim
+++
@@ -1,9 +0,0 @@
-" Remap for Dvorak
-noremap t k
-noremap n j
-noremap s l
-noremap j n
-noremap k s
-
-" Add line numbers
-set number
