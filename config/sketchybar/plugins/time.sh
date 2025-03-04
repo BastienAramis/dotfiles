fddbcd9d188f2d28666f2b76840a62ac62b65ddb
Bastien MUNOZ <bastien.munoz@aramisauto.com>
2025-03-04 14:30:55 +0100

    sketchybar: jp time merged with time

--- config/sketchybar/plugins/time.sh
+++ config/sketchybar/plugins/time.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-LABEL=$(LC_ALL=ja_JP.UTF-8 date +'%-m月%-d日(%a) %H:%M')
+LOCAL_DATE=$(LC_ALL=ja_JP.UTF-8 date +'%-m月%-d日(%a)')
+JP_TIME=$(LC_ALL=ja_JP.UTF-8 TZ='Asia/Tokyo' date +'%H:%M')
+LOCAL_TIME=$(LC_ALL=ja_JP.UTF-8 date +'%H:%M')
+LABEL="$LOCAL_DATE $JP_TIME - $LOCAL_TIME"
 
 sketchybar --set $NAME label="$LABEL" 
 
--- config/sketchybar/plugins/time_jp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-LABEL=$(LC_ALL=ja_JP.UTF-8 TZ='Asia/Tokyo' date +'%-m月%-d日(%a) %H:%M')
-
-sketchybar --set $NAME label="$LABEL" 
-
